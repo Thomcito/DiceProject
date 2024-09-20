4be2e9a2ee51198ebe0c5c91dab337559632ab67
Thomas <thomas.askoe@gmail.com>
2024-09-20 08:37:32 +0200

    Opgave 1 og 2

--- src/opgave01/RollTwoDice.java
+++ src/opgave01/RollTwoDice.java
@@ -1,4 +1,95 @@
 package opgave01;
 
+import java.util.Scanner;
+
 public class RollTwoDice {
+    private static int rollCount = 0;
+    private static int sum = 0;
+    private static int sammeAntalØjne = 0;
+    private static int størsteØjne = 0;
+    private static int[] antaløjne = {1, 2, 3, 4, 5, 6};
+    private static int[] resultatAfØjne = new int[6];
+
+
+    public static void main(String[] args) {
+        System.out.println("Velkommen til spillet, rul to terninger.");
+        printRules();
+        System.out.println();
+
+        playDice();
+
+        System.out.println();
+        System.out.println("Tak for at spille, rul to terninger.");
+    }
+
+    private static void printRules() {
+        System.out.println("=====================================================");
+        System.out.println("Regler for rul to terninger");
+        System.out.println("Spilleren ruller to terninger, så længe man lyster.");
+        System.out.println("=====================================================");
+    }
+
+    public static void playDice() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Rul to terninger? ('ja/nej') ");
+        String answer = scanner.nextLine();
+        while (!answer.equals("nej")) {
+            int[] faces = rollDice();
+            System.out.println("Du rullede: " + faces[0]);
+            System.out.println("Du rullede: " + faces[1]);
+            sum += faces[0] + faces[1];
+
+            if (faces[0] == faces[1]) {
+                sammeAntalØjne++;
+            }
+            if (faces[0] + faces[1] > størsteØjne) {
+                størsteØjne = faces[0] + faces[1];
+            }
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < antaløjne.length; j++) {
+                    if (faces[i] == antaløjne[j]) {
+                        resultatAfØjne[j]++;
+                    }
+                }
+            }
+            updateStatistics();
+
+            System.out.print("Rul to terninger? ('ja/nej') ");
+            answer = scanner.nextLine();
+
+        }
+
+        printStatistics();
+        scanner.close();
+
+    }
+
+    private static int[] rollDice() {
+        int[] dices = new int[2];
+        dices[0] = (int) (Math.random() * 6 + 1);
+        dices[1] = (int) (Math.random() * 6 + 1);
+        return dices;
+    }
+
+
+    private static void updateStatistics() {
+        rollCount++;
+    }
+
+    private static void printStatistics() {
+        System.out.println("\nResults:");
+        System.out.println("-------");
+        System.out.printf("%17s %4d\n", "Antal rul:", rollCount);
+        System.out.println("Den totale sum af alle terningekastene er: " + sum);
+        System.out.println("Du har slået de samme antal øjne " + sammeAntalØjne + " Gange");
+        System.out.println("Det største antal øjne du har slået er: " + størsteØjne);
+        for (int i = 0; i < resultatAfØjne.length; i++) {
+            System.out.println("antal " + antaløjne[i] + "ere = " + resultatAfØjne[i]);
+
+        }
+
+    }
+
 }
+
+
--- src/opgave02/Craps.java
+++ src/opgave02/Craps.java
@@ -1,4 +1,93 @@
 package opgave02;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Craps {
+
+    public static void main(String[] args) {
+        playCraps();
+    }
+
+    public static void playCraps() {
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+        int wins = 0;
+        int losses = 0;
+        boolean spiligen;
+
+        do {
+            System.out.println("Velkommen til Craps!");
+            System.out.println();
+            printrules();
+            System.out.println();
+
+            // Første kast ('come out roll')
+            int førstekast = rollDice(rand);
+            System.out.println("Første kast: " + førstekast);
+
+            if (førstekast == 7 || førstekast == 11) {
+                System.out.println("Du har vundet!");
+                wins++;
+            } else if (førstekast == 2 || førstekast == 3 || førstekast == 12) {
+                System.out.println("Du har tabt!");
+                losses++;
+            } else {
+                int point = førstekast;
+                System.out.println("Dit point er nu: " + point);
+
+                int nytkast;
+                do {
+                    System.out.println("Tryk Enter for at kaste igen...");
+                    scanner.nextLine();
+
+                    nytkast = rollDice(rand);
+                    System.out.println("Du kastede: " + nytkast);
+
+                    if (nytkast == point) {
+                        System.out.println("Du har vundet ved at kaste dit point!");
+                        wins++;
+                        break;
+                    } else if (nytkast == 7) {
+                        System.out.println("Du har tabt ved at kaste 7.");
+                        losses++;
+                        break;
+                    }
+
+                } while (true);  // Kører i loop indtil spilleren vinder eller taber
+            }
+
+            // Spørger spilleren, om de vil spille igen
+            System.out.println("Vil du spille igen? (ja/nej)");
+            String response = scanner.nextLine();
+            spiligen = response.equalsIgnoreCase("ja"); // Så programmet er ligeglad med om inputtet er ja, Ja, jA osv..
+
+        } while (spiligen);
+
+        // Udskriver antal vundne og tabte spil
+        System.out.println("Spillet er slut.");
+        System.out.println("Du har vundet " + wins + " spil.");
+        System.out.println("Du har tabt " + losses + " spil.");
+
+        scanner.close();
+    }
+
+    public static int rollDice(Random rand) {
+        int terning1 = rand.nextInt(6) + 1;
+        int terning2 = rand.nextInt(6) + 1;
+        return terning1 + terning2;
+    }
+
+    public static void printrules() {
+        System.out.println("Det første kast kaldes ‘come out roll’. Spilleren vinder med det samme, hvis det første kast er 7\n" +
+                "eller 11, og taber med det samme, hvis det første kast er 2, 3 eller 12. Hvis spillerens første kast er\n" +
+                "4, 5, 6, 8, 9 eller 10, etableres dette tal som spillerens ‘point’. Spilleren bliver derefter ved med at\n" +
+                "kaste, indtil han enten kaster sit ‘point’ igen eller kaster 7. Kaster han 7, har han tabt. Kaster han\n" +
+                "sit ’point’, har han vundet.");
+
+    }
+
 }
+
+
+
